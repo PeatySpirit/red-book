e837a4df97aabe450d6432fe8d1d0db4f8cf1489
Marek.Rehora <marek.rehora@embedit.cz>
2020-01-06 13:53:05 +0100

    EXERCISE 4.4

--- src/main/scala/org/example/redbook/chapter4/errorhandling/Option.scala
+++ src/main/scala/org/example/redbook/chapter4/errorhandling/Option.scala
@@ -1,34 +1,68 @@
 package org.example.redbook.chapter4.errorhandling
 
-sealed trait Option[+A] {
+sealed trait Option[+Q] {
   //EXERCISE 4.1
-  def map[B](f: A => B): Option[B] = this match {
+  def map[B](f: Q => B): Option[B] = this match {
     case Some(v) => Some(f(v))
     case None    => None
   }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+  def flatMap[B](f: Q => Option[B]): Option[B] = this match {
     case Some(v) => f(v)
     case None    => None
   }
 
-  def getOrElse[B >: A](default: => B): B = this match {
+  def getOrElse[B >: Q](default: => B): B = this match {
     case Some(v) => v
     case None    => default
   }
 
-  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+  def orElse[B >: Q](ob: => Option[B]): Option[B] = this match {
     case Some(v) => Some(v)
     case None    => ob
   }
 
-  def filter(f: A => Boolean): Option[A] = this match {
+  def filter(f: Q => Boolean): Option[Q] = this match {
     case Some(v) if f(v) => this
     case _               => None
   }
+
+  //EXERCISE 4.3
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    (a, b) match {
+      case (Some(aa), Some(bb)) => Some(f(aa, bb))
+      case _                    => None
+    }
+
+  def map2withFlatMap[A, B, C](a: Option[A],
+                               b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap(aa => b.map(bb => f(aa, bb)))
+
+  //EXCERCISE 4.4
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    a match {
+      case Nil    => Some(Nil)
+      case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+    }
+  /*
+  It can also be implemented using `foldRight` and `map2`. The type annotation on `foldRight` is needed here; otherwise
+  Scala wrongly infers the result type of the fold as `Some[Nil.type]` and reports a type error (try it!). This is an
+  unfortunate consequence of Scala using subtyping to encode algebraic data types.
+   */
+  def sequence_2[A](a: List[Option[A]]): Option[List[A]] =
+    a.foldRight[Option[List[A]]](Some(Nil))((x, y) => map2(x, y)(_ :: _))
+
 }
 
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
-object Option {}
+object Option {
+
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
+}
